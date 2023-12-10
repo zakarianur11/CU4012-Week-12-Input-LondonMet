9ba395aca65cb6309effb7fde20601c64054948f
utkarshyadav009 <raouy36@gmail.com>
2023-12-10 16:53:12 +0000

    Initial Commit

---
+++ CU4012-SFML/Level.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include "Framework/Input.h"
+#include <string.h>
+#include <iostream>
+
+
+class Level{
+public:
+	Level(sf::RenderWindow* hwnd, Input* in);
+	~Level();
+
+	void handleInput();
+	void update();
+	void render();
+
+private:
+	// Default functions for rendering to the screen.
+	void beginDraw();
+	void endDraw();
+
+	// Default variables for level class.
+	sf::RenderWindow* window;
+	Input* input;
+
+	// Add your object here
+
+};
